d99b0f689f01deac3ac29684748f37a364b48e6f
ceccobell <francescobell2001@gmail.com>
2024-07-04 14:21:59 +0200

    [UPD] declared function randoNum

--- evenOrOdd/js/script.js
+++ evenOrOdd/js/script.js
@@ -2,3 +2,7 @@ let choice = prompt("Scegli pari o dispari")
 
 let user_num = prompt("Ora inserisci un numero da 1 a 5")
 
+function randomNum() {
+    let num = Math.floor(Math.random() * 5 + 1)
+    return num
+}
