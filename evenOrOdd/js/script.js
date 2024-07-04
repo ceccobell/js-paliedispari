c07ea4ab9528cd7038e610f47578e8f6c06789f2
ceccobell <francescobell2001@gmail.com>
2024-07-04 14:24:15 +0200

    [UPd] declared var for sum

--- evenOrOdd/js/script.js
+++ evenOrOdd/js/script.js
@@ -2,6 +2,10 @@ let choice = prompt("Scegli pari o dispari")
 
 let user_num = prompt("Ora inserisci un numero da 1 a 5")
 
+let pc_num = randomNum()
+
+let num_sum = pc_num + user_num
+
 function randomNum() {
     let num = Math.floor(Math.random() * 5 + 1)
     return num
