1a52fe712e864ece8bf81ff1fdac9684e42acbe9
ceccobell <francescobell2001@gmail.com>
2024-07-04 13:53:32 +0200

    [UPD] created function IsPalindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,12 @@
-alert("TEST")
+function IsPalindroma(word) {
+    let reverse_word = ""
+    for (let i = word.lenght - 1; i >= 0; i--;) {
+        reverse_word += word[i]
+    }
+
+    if (word == reverse_word) {
+        return true
+    }
+
+    return false
+}
